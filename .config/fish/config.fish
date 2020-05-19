fd6cffa78e46887e9232314ba4d2bf4f3e8b24ed
hanbeul <han310@gmail.com>
2020-05-18 22:48:24 -0400

    Updated various linux config files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,6 @@
-set fish_user_paths $HOME/.bin $HOME/.npm-global/bin /snap/bin
+set fish_user_paths $HOME/.bin $HOME/.npm-global/bin /var/lib/snapd/snap/bin /opt/cuda/bin
 set NPM_CONFIG_PREFIX $HOME/.npm-global
 set EDITOR vim
 
 function fish_greeting
-end
-
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
 end
