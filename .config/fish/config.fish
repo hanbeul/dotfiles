8c8453fa7d8030210f69dc120dba9a9d347ac054
hanbeul <han310@gmail.com>
2021-01-14 16:54:39 -0500

    i3wm: Removed window border on focused window. Polybar: Enabled ipc
    Enabling ipc allows polybar to restart in place with 'polybar-msg cmd restart'

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 set EDITOR vim
-
 set NPM_CONFIG_PREFIX $HOME/.npm-global
-
 set GOPATH $HOME/.go
+
+alias vim nvim
 
 set fish_user_paths $HOME/.bin $HOME/.npm-global/bin /var/lib/snapd/snap/bin /opt/cuda/bin $GOPATH
 
