d389e53a0eac56b3e0034efffe6d6d7524f98adc
hanbeul <han310@gmail.com>
2021-02-27 00:26:09 -0500

    Updated fish path with cargo path. Disable dpms. Changed nautilus start command in i3wm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ set GOPATH $HOME/.go
 
 alias vim nvim
 
-set fish_user_paths $HOME/.bin $HOME/.npm-global/bin /var/lib/snapd/snap/bin /opt/cuda/bin $GOPATH
+set fish_user_paths $HOME/.bin $HOME/.npm-global/bin /var/lib/snapd/snap/bin /opt/cuda/bin $GOPATH $HOME/.cargo/bin
 
 function fish_greeting
 end
