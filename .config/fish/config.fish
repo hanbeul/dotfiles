41c1db3bd3c11f21b44729d87f6e7aa920a3d082
hanbeul <han310@gmail.com>
2021-06-15 23:05:52 -0400

    Updated a bunch of stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,11 @@ alias vim nvim
 
 set fish_user_paths $HOME/.bin $HOME/.npm-global/bin /var/lib/snapd/snap/bin /opt/cuda/bin $GOPATH $HOME/.cargo/bin
 
+if test -z (pgrep ssh-agent)
+  eval (ssh-agent -c) > /dev/null
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
+
 function fish_greeting
 end
