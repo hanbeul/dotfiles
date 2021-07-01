22ecefc01c7079974b6aa2c80382da316900e03a
hanbeul <han310@gmail.com>
2021-07-01 00:37:26 -0400

    Added new mouse to scripts, for mouse acceleration. Updated volume control scripts to use pactl. Changed polybar to use built in volume module, for now.

--- .bin/udev-keyboard.sh
+++ .bin/udev-keyboard.sh
@@ -5,3 +5,4 @@ XAUTHORITY=/home/hanbeul/.Xauthority
 DISPLAY=:0 XAUTHORITY=/home/hanbeul/.Xauthority setxkbmap -option caps:none
 DISPLAY=:0 XAUTHORITY=/home/hanbeul/.Xauthority /usr/bin/xset r rate 200 25
 DISPLAY=:0 XAUTHORITY=/home/hanbeul/.Xauthority /usr/bin/xinput --set-prop "Logitech Gaming Mouse G502" "libinput Accel Profile Enabled" 0, 1
+DISPLAY=:0 XAUTHORITY=/home/hanbeul/.Xauthority /usr/bin/xinput --set-prop "Logitech USB Receiver" "libinput Accel Profile Enabled" 0, 1
