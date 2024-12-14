2f057cabb366803dfa1afa387ab7c75e7df32a40
hanbeul <han310@gmail.com>
2024-12-14 15:54:41 -0500

    Updated nvim.lua and added i3 workspaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', {})
 vim.g.mapleader=" "
 vim.api.nvim_set_keymap('n', '<c-k>', '<c-y>', {})
@@ -15,211 +19,290 @@ vim.opt.number = true
 vim.opt.smartcase = true
 vim.opt.ignorecase = true
 
+-- 2 Spaces per tab
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 local uv = vim.uv or vim.loop
 
 -- Autoinstall lazy nvim if not present
 if not uv.fs_stat(lazypath) then
-	print('Installing lazy.nvim....')
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release,
-		lazypath,
-	})
-	print('Done.')
+  print('Installing lazy.nvim....')
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release,
+    lazypath,
+  })
+  print('Done.')
 end
 
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
-	{'dracula/vim',
-		config = function()
-			-- Set colorscheme
-			vim.opt.termguicolors = true
-			vim.cmd.colorscheme('dracula')
-		end
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local treesitter = require("nvim-treesitter.configs")
-			treesitter.setup({
-				highlight = {
-					enable = true,
-				},
-				indent = {
-					enable = true,
-				}
-			})
-		end
-	},
-	{ "lukas-reineke/indent-blankline.nvim",main = "ibl", opts = {}},
-	{ "alexghergh/nvim-tmux-navigation", config = function()
-		local nvim_tmux_nav = require('nvim-tmux-navigation')
-		nvim_tmux_nav.setup {
-			disable_when_zoomed = false -- defaults to false
-		}
+  {'dracula/vim',
+    config = function()
+      -- Set colorscheme
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme('dracula')
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+      treesitter.setup({
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        }
+      })
+    end
+  },
+  { "lukas-reineke/indent-blankline.nvim",main = "ibl", opts = {}},
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>-",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory" ,
+      },
+      {
+        -- NOTE: this requires a version of yazi that includes
+        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+        '<c-up>',
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    ---@type YaziConfig
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = '<f1>',
+      },
+    },
+  },
+  { "alexghergh/nvim-tmux-navigation", config = function()
+    local nvim_tmux_nav = require('nvim-tmux-navigation')
+    nvim_tmux_nav.setup {
+      disable_when_zoomed = false -- defaults to false
+    }
 
-		vim.keymap.set('n', '<A-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
-		vim.keymap.set('n', '<A-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
-		vim.keymap.set('n', '<A-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
-		vim.keymap.set('n', '<A-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
+    vim.keymap.set('n', '<A-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
+    vim.keymap.set('n', '<A-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
+    vim.keymap.set('n', '<A-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
+    vim.keymap.set('n', '<A-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
 
-		vim.keymap.set('n', '˙', nvim_tmux_nav.NvimTmuxNavigateLeft)
-		vim.keymap.set('n', '∆', nvim_tmux_nav.NvimTmuxNavigateDown)
-		vim.keymap.set('n', '˚', nvim_tmux_nav.NvimTmuxNavigateUp)
-		vim.keymap.set('n', '¬', nvim_tmux_nav.NvimTmuxNavigateRight)
-	end
-	},
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end
-	},
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.6',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			require('telescope').setup{
-				defaults = {
-					-- Default configuration for telescope goes here:
-					-- config_key = value,
-					mappings = {
-						i = {
-							-- map actions.which_key to <C-h> (default: <C-/>)
-							-- actions.which_key shows the mappings for your picker,
-							-- e.g. git_{create, delete, ...}_branch for the git_branches picker
-							["<C-j>"] = require('telescope.actions').move_selection_next,
-							["<C-k>"] = require('telescope.actions').move_selection_previous
-						}
-					}
-				},
-				pickers = {
-					-- Default configuration for builtin pickers goes here:
-					-- picker_name = {
-					--   picker_config_key = value,
-					--   ...
-					-- }
-					-- Now the picker_config_key will be applied every time you call this
-					-- builtin picker
-				},
-				extensions = {
-					-- Your extension configuration goes here:
-					-- extension_name = {
-					--   extension_config_key = value,
-					-- }
-					-- please take a look at the readme of the extension you want to configure
-				}
-			}
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		end
-	},
-	{
-		'romgrk/barbar.nvim',
-		dependencies = {
-			'lewis6991/gitsigns.nvim',
-			'nvim-tree/nvim-web-devicons',
-		},
-		init = function() vim.g.barbar_auto_setup = false end,
-		opts = {
-			animation = true,
-		}
-	},
-	{
-		'nvim-tree/nvim-tree.lua',
-		opts = {
-			on_attach = on_attach_change
-		}
-	},
-	{
-		'nvim-tree/nvim-tree.lua',
-		opts = { on_attach = on_attach_change }
-	},
-	{
-		'williamboman/mason.nvim',
-		lazy = false,
-		config = function()
-			require('mason').setup()
-		end,
-	},
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		lazy = true,
-		config = false,
-		init = function()
-			vim.g.lsp_zero_extend_cmp = 0
-			vim.g.lsp_zero_extend_lspconfig = 0
-		end,
-	},
-	{
-		'jiaoshijie/undotree',
-		dependencies = 'nvim-lua/plenary.nvim',
-		config = true,
-		keys = {
-			{ '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
-		},
-	},
-	{
-		'hrsh7th/nvim-cmp',
-		event = 'InsertEnter',
-		dependencies = {
-			{ 'L3MON4D3/LuaSnip' },
-		},
-		config = function()
-			local lsp_zero = require('lsp-zero')
-			lsp_zero.extend_cmp()
+    vim.keymap.set('n', '˙', nvim_tmux_nav.NvimTmuxNavigateLeft)
+    vim.keymap.set('n', '∆', nvim_tmux_nav.NvimTmuxNavigateDown)
+    vim.keymap.set('n', '˚', nvim_tmux_nav.NvimTmuxNavigateUp)
+    vim.keymap.set('n', '¬', nvim_tmux_nav.NvimTmuxNavigateRight)
+  end
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup{
+        defaults = {
+          -- Default configuration for telescope goes here:
+          -- config_key = value,
+          mappings = {
+            i = {
+              -- map actions.which_key to <C-h> (default: <C-/>)
+              -- actions.which_key shows the mappings for your picker,
+              -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+              ["<C-j>"] = require('telescope.actions').move_selection_next,
+              ["<C-k>"] = require('telescope.actions').move_selection_previous
+            }
+          }
+        },
+        pickers = {
+          -- Default configuration for builtin pickers goes here:
+          -- picker_name = {
+          --   picker_config_key = value,
+          --   ...
+          -- }
+          -- Now the picker_config_key will be applied every time you call this
+          -- builtin picker
+        },
+        extensions = {
+          -- Your extension configuration goes here:
+          -- extension_name = {
+          --   extension_config_key = value,
+          -- }
+          -- please take a look at the readme of the extension you want to configure
+        }
+      }
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
+  },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      animation = true,
+    }
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = { on_attach = on_attach_change }
+  },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = function()
+      require('mason').setup()
+    end,
+  },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+    init = function()
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
+    end,
+  },
+  {
+    'jiaoshijie/undotree',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = true,
+    keys = {
+      { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      { 'L3MON4D3/LuaSnip' },
+    },
+    config = function()
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_cmp()
 
-			local cmp = require('cmp')
-			local cmp_action = lsp_zero.cmp_action()
+      local cmp = require('cmp')
+      local cmp_action = lsp_zero.cmp_action()
 
-			cmp.setup({
-				formatting = lsp_zero.cmp_format(),
-				mapping = cmp.mapping.preset.insert({
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-u>'] = cmp.mapping.scroll_docs(-4),
-					['<C-d>'] = cmp.mapping.scroll_docs(4),
-					['<C-f>'] = cmp_action.luasnip_jump_forward(),
-					['<C-f>'] = cmp_action.luasnip_jump_backward(),
-				})
-			})
-		end
-	},
-	{
-		'neovim/nvim-lspconfig',
-		cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-		event = { 'BufReadPre', 'BufNewFile' },
-		dependencies = {
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'williamboman/mason-lspconfig.nvim' },
-		},
-		config = function()
-			local lsp_zero = require('lsp-zero')
-			lsp_zero.extend_lspconfig()
+      cmp.setup({
+        formatting = lsp_zero.cmp_format(),
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-f>'] = cmp_action.luasnip_jump_backward(),
+        })
+      })
+    end
+  },
+  {
+    'neovim/nvim-lspconfig',
+    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'williamboman/mason-lspconfig.nvim' },
+    },
+    config = function()
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
 
-			lsp_zero.on_attach(function(client, bufnr)
-				lsp_zero.default_keymaps({buffer = bufnr})
-			end)
+      lsp_zero.on_attach(function(client, bufnr)
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
 
-			require('mason-lspconfig').setup({
-				ensure_installed = {},
-				handlers = {
-					lsp_zero.default_setup
-				}
-			})
-		end
-	}
+      require('mason-lspconfig').setup({
+        ensure_installed = {},
+        handlers = {
+          lsp_zero.default_setup
+        }
+      })
+    end
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  }
 })
